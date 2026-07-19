728e0511b84dac5a3cff179ba60a1b84c6cef5e0
Prohovoch <w1ndoff@yandex.ru>
2026-07-19 14:50:00 +0700

    Update:
    - Auth entity model.

---
+++ OrderService/Infrastructure/Entities/Auth/TokenModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection.Metadata;
+
+namespace OrderService.Infrastructure.Entities.Auth
+{
+    public enum TokenType
+    {
+       Admin, Worker
+    }
+    public class TokenModel
+    {
+        public Guid Id { get; set; } = Guid.CreateVersion7();
+        public string TokenHash { get; set; } = string.Empty;
+        public bool IsUsed { get; set; }
+        public TokenType Type { get; set; }
+    }
+}
