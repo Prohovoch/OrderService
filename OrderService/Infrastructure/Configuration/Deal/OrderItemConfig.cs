9444def631c5deb444f993cb209bc926d5ef82a5
Prohovoch <w1ndoff@yandex.ru>
2026-07-22 14:09:28 +0700

    update:
    - order item config first iteration

---
+++ OrderService/Infrastructure/Configuration/Deal/OrderItemConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderService.Infrastructure.Entities.Deal;
+
+
+
+namespace OrderService.Infrastructure.Configuration.Deal
+{
+    public class OrderItemConfig : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.ToTable("order_items");
+            builder.HasKey(oi => oi.Id);
+            builder.ComplexProperty(oi => oi.Details, oid => oid.ToJson());
+            
+                
+            builder.Property(oi => oi.Id).HasColumnName("id").ValueGeneratedOnAdd();
+            
+
+        }
+    }
+}
