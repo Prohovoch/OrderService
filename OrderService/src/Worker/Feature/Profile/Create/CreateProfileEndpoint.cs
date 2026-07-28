c975823371a574417ccbabd65b0a124b8f513a0c
Prohovoch <w1ndoff@yandex.ru>
2026-07-28 15:51:05 +0700

    update:
    employee profile creation feature.

---
+++ OrderService/src/Worker/Feature/Profile/Create/CreateProfileEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using FastEndpoints;
+using OrderService.Infrastructure.Persistence;
+using OrderService.Infrastructure.Entities.Employee;
+namespace OrderService.src.Worker.Feature.Profile.Create
+{
+    // REPR endpoint
+    public class CreateProfileEndpoint(ApplicationDbContext dbContext) : EndpointWithMapper<CreateProfileRequest, ProfileMapper>
+    {
+
+        public readonly ApplicationDbContext _dbContext = dbContext;
+
+        public override void Configure()
+        {
+            Post("api/employee/profile");
+            Roles("worker");
+            Validator<ValidatorClass>();
+
+        }
+
+
+        public override async Task HandleAsync(CreateProfileRequest req, CancellationToken ct)
+        {
+            WorkerProfile entity = Map.ToEntity(req);
+
+            _dbContext.Add(entity);
+            await _dbContext.SaveChangesAsync();
+            await Send.OkAsync();
+        }
+    }
+}
---
+++ OrderService/src/Worker/Feature/Profile/Create/Mapper.cs
@@ -0,0 +1,24 @@
+﻿using FastEndpoints;
+using OrderService.Infrastructure.Entities.Employee;
+namespace OrderService.src.Worker.Feature.Profile.Create
+{
+    // alias
+    using WorkerGender = OrderService.Infrastructure.Entities.Employee.Gender;
+
+    public class ProfileMapper : RequestMapper<CreateProfileRequest, WorkerProfile>
+    {
+        public override WorkerProfile ToEntity(CreateProfileRequest r) => new()
+        {
+            WorkerId = r.UserId,
+            Name = r.Name,
+            Surname = r.Surname,
+            Age = r.Age,
+            Gender = r.Gender switch
+            {
+                Gender.Male => WorkerGender.Male,
+                Gender.Female => WorkerGender.Female,
+                _ => null
+            }
+        };
+    }
+}
