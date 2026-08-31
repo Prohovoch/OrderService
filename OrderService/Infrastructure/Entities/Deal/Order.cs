b1c8d6f5c5931a1a553537ea68ef3853fe8306f4
Prohovoch <w1ndoff@yandex.ru>
2026-09-01 02:34:35 +0700

    update:
    TODO: change job with phone + adress.
    
    think about  error validation (availability)

--- OrderService/Infrastructure/Entities/Deal/Order.cs
+++ OrderService/Infrastructure/Entities/Deal/Order.cs
@@ -1,5 +1,6 @@
 ﻿using OrderService.Infrastructure.Entities.Employee;
-using OrderService.Infrastructure.Entities.Buyer; 
+using OrderService.Infrastructure.Entities.Buyer;
+using System.Diagnostics.CodeAnalysis;
 
 namespace OrderService.Infrastructure.Entities.Deal
 {
@@ -21,8 +22,10 @@ namespace OrderService.Infrastructure.Entities.Deal
         public Guid? WorkerId { get; set; }
         public Customer? Customer { get; set; }
         public Worker? Worker { get; set; }
-        public List<OrderItem> Items { get; } = [];
+        public required string CustomerPhoneNumber { get; set; }
 
+        public List<OrderItem> Items { get; } = [];
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
         public OrderStatus Status { get; set; }
         
 
